44e434ce65966904e4e5f3065e95756c396e3a6b
Tenderfeel <tenderfeel@gmail.com>
2025-10-19 23:06:05 +0900

    chore(types): Update global type definitions for Google Tag Manager
    - Refactor Window interface type definitions
    - Change `dataLayer` type from `Record<string, any>[]` to `Record<string, unknown>[]`
    - Update `gtag` function parameter type from `any[]` to `unknown[]`
    - Improve type safety for global window properties

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -4,7 +4,7 @@ export * from "@submodule/zzz-wiki-scrap/src/types";
 // Google Analytics用のグローバル型定義
 declare global {
   interface Window {
-    dataLayer: Record<string, any>[];
-    gtag: (...args: any[]) => void;
+    dataLayer: Record<string, unknown>[];
+    gtag: (...args: unknown[]) => void;
   }
 }
